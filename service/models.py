944cb10c2c5d8f753e2f6652c9f7d86e09c8afb1
NouKD <NouKD-n09916538@gmail.com>
2020-11-11 23:58:05 +0000

    standar

--- service/models.py
+++ service/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SocialAccount(models.Model):
+    ICONS = [
+        ("facebook", "fa-facebook-f"),
+        ("instagram", "fa-instagram"),
+        ("google-plus", "fa-google-plus-g"),
+        ("linkedin", "fa-linkedin-in")
+    ]
+    
+    nom = models.CharField(max_length=255)
+    lien = models.URLField()
+    icon = models.CharField(choices=ICONS, max_length=20)
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Social account'
+        verbose_name_plural = 'Socials account'
+
+    def __str__(self):
+        return self.nom
+
+
+
+class SiteInfo(models.Model):
+    map_url = models.TextField()
+    email = models.EmailField()
+    logo = models.ImageField(upload_to="images/SiteInfo")
+    adresse = models.CharField(max_length=255, null=True)
+    tel = models.CharField(max_length=255, null=True)
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Site info'
+        verbose_name_plural = 'Site infos'
+
+    def __str__(self):
+        return self.email
+
+
+
+class Presentation(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/Presentation")
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Presentation'
+        verbose_name_plural = 'Presentations'
+
+    def __str__(self):
+        return self.nom
